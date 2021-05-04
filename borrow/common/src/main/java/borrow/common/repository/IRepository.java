e28fcea828af3c94e8e42b7bb9c27b67b73e9d54
Henrique A. Richter <henrique.a.richter@gmail.com>
2021-05-04 20:47:08 -0300

    Add findByKeyId

--- borrow/common/src/main/java/borrow/common/repository/IRepository.java
+++ borrow/common/src/main/java/borrow/common/repository/IRepository.java
@@ -1,6 +1,7 @@
 package borrow.common.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
@@ -9,6 +10,7 @@ import org.springframework.data.repository.NoRepositoryBean;
 @NoRepositoryBean
 public interface IRepository<T> extends CassandraRepository<T, UUID> {
   
+  Optional<T> findByKeyId(UUID id);
   List<T> findAllByKeyId(UUID id);
   List<T> findByName(String name);
   List<T> findByNameContaining(String name);
