00984c94b9f6e54e6543dbd465b2899bd1b8cbad
Henrique A. Richter <henrique.a.richter@gmail.com>
2021-05-06 23:06:34 -0300

    Add hashCode and equals

--- borrow/common/src/main/java/borrow/common/entity/Entity.java
+++ borrow/common/src/main/java/borrow/common/entity/Entity.java
@@ -62,4 +62,47 @@ public class Entity {
         + updatedAt + "";
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+    result = prime * result + ((description == null) ? 0 : description.hashCode());
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Entity other = (Entity) obj;
+    if (createdAt == null) {
+      if (other.createdAt != null)
+        return false;
+    } else if (!createdAt.equals(other.createdAt))
+      return false;
+    if (description == null) {
+      if (other.description != null)
+        return false;
+    } else if (!description.equals(other.description))
+      return false;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    if (updatedAt == null) {
+      if (other.updatedAt != null)
+        return false;
+    } else if (!updatedAt.equals(other.updatedAt))
+      return false;
+    return true;
+  }
+
 }
